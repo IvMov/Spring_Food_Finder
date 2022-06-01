e06ce19c66ae6e13cc50be56c5438bcca8d971d2
ivmov <ivmovchanets@gmail.com>
2022-06-01 16:03:38 +0300

    initial commit

---
+++ src/test/java/iv/movchanets/foodfinder/FoodfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package iv.movchanets.foodfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
